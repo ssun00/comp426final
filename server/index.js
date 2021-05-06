246549bf9d2a48a25de97db82cc4958ea45bb1e4
Sophie Sun <ssun00@live.unc.edu>
2021-05-06 19:53:55 -0400

    Added app listener

--- server/index.js
+++ server/index.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// app.listen(PORT, () => {
-//   console.log(`Server listening on ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
